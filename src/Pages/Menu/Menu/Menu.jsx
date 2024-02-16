1f8e1e189f287ca46f8111ef0563a5453a5b8ac5
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-16 11:59:23 +0600

    cover wiht react parallax and menu section complete

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,21 @@
 import {  Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopulerMenu from '../../Home/PopulerMenu/PopulerMenu';
 
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
-        
       </Helmet>
-      <h2>Menu</h2>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopulerMenu></PopulerMenu>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopulerMenu></PopulerMenu>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopulerMenu></PopulerMenu>
+     
     </div>
   );
 };
