5cce43992f0b1c16f092cd19a48f82fdeb27c6c7
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-19 02:19:00 +0600

    FoodCard OrderCard added

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import orderCoverImg from '../../../assets/shop/order.jpg'
 import Cover from '../../Shared/Cover/Cover';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../../Hooks/useMenu';
 
 const Order = () => {
   const [tabIndex, setTabIndex] = useState(0);
+  const [menu] =useMenu();
+  const desserts = menu.filter (item =>item.category === 'dessert')   
+  const soup = menu.filter (item =>item.category === 'soup')   
+  const salad = menu.filter (item =>item.category === 'salad')   
+  const pizza = menu.filter (item =>item.category === 'pizza')   
+  const offered = menu.filter (item =>item.category === 'offered')  
+
   return (
     <div>
       <Cover img={orderCoverImg} title={"Order Food"}></Cover>
