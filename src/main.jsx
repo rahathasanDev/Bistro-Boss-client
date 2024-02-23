ff9aba59175c18a82c6466d6bc8a5a1cfafaf6dd
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-24 02:05:15 +0600

    firebase setup for bistro boss

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,17 @@ import {
 import { router } from './Routes/Routes.jsx';
 // react helmet title route
 import { HelmetProvider } from 'react-helmet-async';
+import Authprovider from './providers/Authprovider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl	mx-auto '>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <Authprovider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl	mx-auto '>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </Authprovider>
   </React.StrictMode>,
 )
