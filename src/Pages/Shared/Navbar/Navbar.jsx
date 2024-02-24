07d5311120b340b3f25b8d3835c05badb52e4c6b
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-25 03:09:40 +0600

    Logout, Sweet Alert And Some Bug Fix

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,41 +1,60 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/Authprovider";
 
 
 const Navbar = () => {
-  
-  const navOptions = <>
-  <li className=" hover:outline outline-1 rounded-lg "><Link to="/">Home</Link></li>
-  <li><Link to="/menu">Our Menu</Link></li>
-  <li><Link to="/order/salad">Order Food</Link></li>
-  <li><Link to="/login">Login</Link></li>
- 
-  
-</>
-  return (
-    <>
-    <div className="navbar fixed  z-10 bg-opacity-10 bg-black font-medium max-w-screen-xl">
-        <div className="navbar-start">
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    {navOptions}
-                </ul>
+    const { user, logOut } = useContext(AuthContext);
+
+    const handlelogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error));
+
+    }
+    const navOptions = <>
+        <li className=" hover:outline outline-1 rounded-lg "><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
+
+        {
+            user ?
+                <>
+                    <button onClick={handlelogOut} className="btn btn-outline ">Default
+                    </button>
+                </> :
+                <>
+                    <li><Link to="/login">Login</Link></li>
+                </>
+        }
+
+
+    </>
+    return (
+        <>
+            <div className="navbar fixed  z-10 bg-opacity-10 bg-black font-medium max-w-screen-xl">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Get started</a>
+                </div>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                {navOptions}
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <a className="btn">Get started</a>
-        </div>
-    </div>
-</>
-  );
+        </>
+    );
 };
 
 export default Navbar;
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
 import { AuthContext } from "../../providers/Authprovider";
 
 const SignUp = () => {
@@ -20,6 +21,13 @@ const SignUp = () => {
                     .then(() => {
                         console.log('user profile info updated')
                         reset();
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'User created successfully.',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
                         navigate('/');
 
                     })
