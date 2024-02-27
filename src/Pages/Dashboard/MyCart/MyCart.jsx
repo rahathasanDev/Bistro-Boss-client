f000f0e982017e11a90b8315c6632082961a6c0e
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-28 02:10:09 +0600

    Cart data from custom hook and calculate total price

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import useCart from '../../../Hooks/useCart';
 
 const MyCart = () => {
+  const [cart] = useCart();
+  console.log(cart);
+  const total = cart.reduce((sum,item)=>item.price + sum,0)
   return (
-  <div>
+    <div className='uppercase'>
       <Helmet>
         <title>Bistro Boss | MyCart</title>
       </Helmet>
-    <h2>My Cart</h2>
-  </div>
+      <div>
+        <h3 className="text-3xl">Total Items: {cart.length}</h3>
+        <h3 className="text-3xl toFixed(2)">Total Price: {total.toFixed(2)}</h3>
+        <button className="btn w-28 btn-warning">Pay</button>
+      </div>
+    </div>
   );
 };
 
