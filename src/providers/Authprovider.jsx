bd7f946be97f53c866cb98b2e908fb9282db70db
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-03 03:43:49 +0600

    Send JWT token to back end for verification

--- src/providers/Authprovider.jsx
+++ src/providers/Authprovider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -38,6 +39,21 @@ const Authprovider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser);
       console.log('Current user', currentUser);
+      // get and set token
+      if(currentUser){
+        axios.post('http://localhost:5000/jwt' , {email: currentUser.email})
+        .then(data=>{
+          // console.log(data.data.token);
+          localStorage.setItem('access-token', data.data.token)
+        })
+      }
+      else {
+        localStorage.removeItem('access-token')
+      }
+
+
+
+
       setLoading(false);
     });
     return () => {
