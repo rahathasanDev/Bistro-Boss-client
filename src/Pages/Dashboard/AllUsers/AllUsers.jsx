ba7fd8e95ed416e3c9e450564215f7e48aba06d4
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-03 02:12:44 +0600

    Make User Admin using patch

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 
 const AllUsers = () => {
@@ -11,53 +12,73 @@ const AllUsers = () => {
       return res.json();
     }
   });
+
+  const handleMakeAdmin =user =>{
+    fetch(`http://localhost:5000/users/admin/${user._id}`,{
+      method: 'PATCH'
+    })
+    .then(res =>res.json())
+    .then(data => {
+      console.log(data)
+      if(data.modifiedCount){
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
+  }
   return (
     <div>
-        <div className="flex justify-evenly my-4">
-            <h2 className="text-3xl">All Users</h2>
-            <h2 className="text-3xl">Total Users: {users.length}</h2>
-        </div>
-        <div className="overflow-x-auto">
-            <table className="table table-zebra w-full">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.map((user, index) => <tr key={user._id}>
-                            <th>{index + 1}</th>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>
-                                { user.role === 'admin' ? 'Admin' : <button
-                                    // onClick={() => handleMakeAdmin(user)}
-                                    className="btn  bg-orange-500">
-                                    <FaUsers className="text-white 
+      <div className="flex justify-evenly my-4">
+        <h2 className="text-3xl">All Users</h2>
+        <h2 className="text-3xl">Total Users: {users.length}</h2>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user, index) => <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === 'admin' ? 'Admin' : <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn  bg-orange-500">
+                    <FaUsers className="text-white 
                                     text-2xl"></FaUsers>
-                                </button>}
-                            </td>
-                            <td>
-                                <button
-                                    // onClick={() => handleDeleteUser(user)}
-                                    className="btn btn-ghost btn-lg">
-                                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
-                                </button>
-                            </td>
-                        </tr>)
-                    }
+                  </button>}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user)}
+                    className="btn btn-ghost btn-lg">
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>)
+            }
 
-                </tbody>
-            </table>
-        </div>
+          </tbody>
+        </table>
+      </div>
     </div>
-);
+  );
 };
 
 export default AllUsers;
