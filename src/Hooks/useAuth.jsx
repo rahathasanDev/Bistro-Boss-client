2db204c9c10df7f3765d44302d5be5239b169269
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-03 19:50:13 +0600

    (advanced) axios interceptor

--- src/Hooks/useAuth.jsx
+++ src/Hooks/useAuth.jsx
@@ -1,10 +1,11 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../providers/Authprovider";
+import { useContext } from "react";
+import { AuthContext } from "../providers/Authprovider";
 
 
-// const useAuth = () => {
-//     const auth = useContext(AuthContext);
-//     return auth;
-// };
 
-// export default useAuth;
+const useAuth = () => {
+    const auth = useContext(AuthContext);
+    return auth;
+};
+
+export default useAuth;
