cdc41ae86603dc45a7be0696a8320ba8f8506835
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-09 02:11:40 +0600

    store images with pros and cons

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,13 +1,52 @@
+import { useForm } from 'react-hook-form';
+import Swal from "sweetalert2";
 
-import Sectiontitle from "../../../components/SectionTitle/SectionTitle";
-
-
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 
 const AddItem = () => {
+  
+  const { register, handleSubmit, reset } = useForm();
+  const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+
+  const onSubmit = data => {
+
+    const formData = new FormData();
+    formData.append('image', data.image[0])
+
+    fetch(img_hosting_url, {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(imgResponse => {
+        if (imgResponse.success) {
+          const imgURL = imgResponse.data.display_url;
+          const { name, price, category, recipe } = data;
+          const newItem = { name, price: parseFloat(price), category, recipe, image: imgURL }
+          console.log(newItem)
+          axiosSecure.post('/menu', newItem)
+            .then(data => {
+              console.log('after posting new menu item', data.data)
+              if (data.data.insertedId) {
+                reset();
+                Swal.fire({
+                  position: 'top-end',
+                  icon: 'success',
+                  title: 'Item added successfully',
+                  showConfirmButton: false,
+                  timer: 1500
+                })
+              }
+            })
+        }
+      })
+
+  };
+
+
   return (
     <div className="w-full px-10">
-      <Sectiontitle subHeading="What's new" heading="Add an item" ></Sectiontitle>
-      <form >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control w-full mb-4">
           <label className="label">
             <span className="label-text font-semibold">Recipe Name*</span>
@@ -42,13 +81,13 @@ const AddItem = () => {
           <label className="label">
             <span className="label-text">Recipe Details</span>
           </label>
-          <textarea {("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+          <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
         </div>
         <div className="form-control w-full my-4">
           <label className="label">
             <span className="label-text">Item Image*</span>
           </label>
-          <input type="file" {("image", { required: true })} className="file-input file-input-bordered w-full " />
+          <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full " />
         </div>
         <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
       </form>
